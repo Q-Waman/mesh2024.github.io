c2e44f6f4ae0896408801e243a5d55efcecfce19
Daniel Miguel Huaman Molina <141707244+Q-Waman@users.noreply.github.com>
2024-04-20 13:44:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-$('.carousel').carousel();
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-item");
+    if (n >= slides.length) {
+        slideIndex = 0
+    }    
+    if (n < 0) {
+        slideIndex = slides.length - 1
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex].style.display = "block";  
+}
+
