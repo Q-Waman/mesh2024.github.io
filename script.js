e0a51e74af7c07d406c3d890a3a2a2741f7073e7
Daniel Miguel Huaman Molina <141707244+Q-Waman@users.noreply.github.com>
2024-04-20 13:38:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('aside nav ul li a');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetId = event.target.getAttribute('data-target');
+            var sections = document.querySelectorAll('main section');
+            
+            // Oculta todas las secciones
+            sections.forEach(function (section) {
+                section.style.display = 'none';
+            });
+
+            // Muestra la sección seleccionada
+            var targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.style.display = 'block';
+            }
+        });
+    });
+});
+
+
+
 $('.carousel').carousel();
